70da911cf9452eb22e100ab116e4a40e8c0cf976
faizan <falam9425@gmail.com>
2022-09-23 16:43:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Form } from "./components/Form";
+
+function App() {
+
+  return (
+    <div className="wrapper">
+      <h1>NOVA LIFESTYLE</h1><br></br><br></br>
+      <h3>Add tour text hear</h3><br></br><br></br><br></br>
+      <div className="form-and-todo-box">
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
